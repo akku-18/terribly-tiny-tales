11e7edb66649c303530f4c010d63f604de02f4c0
akku-18 <akshanshgarg18@gmail.com>
2023-05-08 20:28:56 +0530

    fetched the content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fileContents, setFileContents] = useState('');
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://www.terriblytinytales.com/test.txt');
+    const contents = await response.text();
+    setFileContents(contents);
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
-      
+      <button onClick={handleSubmit}>Submit</button>
+      {isLoading ? <p>Loading file...</p> : <p>{fileContents}</p>}
     </div>
   );
 }
